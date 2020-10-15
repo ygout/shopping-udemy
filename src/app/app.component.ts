ef4fba7d93329ac182697275e05d47b5fb340ef0
Yohann <yohann.goutaret@gfi.fr>
2020-10-15 14:49:25 +0200

    add http client module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title: string = 'client';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (response: IPagination) => {
+        this.products = response.data;
+        console.log('response', response);
+      },
+      (error) => {
+        console.log('error', error);
+      }
+    );
+  }
 }
